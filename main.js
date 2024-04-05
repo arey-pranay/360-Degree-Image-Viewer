32901c904445972500d899e5e9e9086edaa6a223
arey-pranay <pranayparikh2004@gmail.com>
2024-04-05 19:34:43 +0530

    added tailwind and responsiveness..

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
+// main.js
+
+const panoramaImage = new PANOLENS.ImagePanorama("images/image2.jpg");
+const panoramaImage2 = new PANOLENS.ImagePanorama("images/image1.jpeg");
+
 const imageContainer = document.querySelector(".image-container");
+
+const btns = document.querySelectorAll(".btn, .btn2");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (btn.classList.contains("btn")) {
+      yesRoom();
+    } else if (btn.classList.contains("btn2")) {
+      noRoom();
+    }
+  });
+});
 
 const viewer = new PANOLENS.Viewer({
   container: imageContainer,
   autoRotate: true,
-  autoRotateSpeed: 0.3,
-  controlBar: false,
+  autoRotateSpeed: 0.6,
+  controlBar: true,
 });
-
-viewer.add(panoramaImage);
+function yesRoom() {
+  viewer.add(panoramaImage);
+}
+function noRoom() {
+  // viewer.remove(panoramaImage2);
+  // viewer.remove(panoramaImage2);
+  viewer.remove(panoramaImage);
+}
+// viewer.add(panoramaImage);
